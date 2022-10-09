db1a8ebac4c4d6e47ea1d45f403efd9378e7a1ed
Devansh Rautela <maildevanshrautela@gmail.com>
2022-10-09 17:49:27 +0530

    commit

--- pattern/pattern2.cpp
+++ pattern/pattern2.cpp
@@ -12,23 +12,23 @@ int main()
     }
 }
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
 void pattern(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<i+1; j++)
+        for (int j = 0; j < i + 1; j++)
         {
-            cout<<"* ";
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     pattern(n);
     return 0;
 }
