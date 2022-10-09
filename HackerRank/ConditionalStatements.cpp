8fc8992f0ba8ced67ea28afea1bb828d76166f00
Devansh Rautela <maildevanshrautela@gmail.com>
2022-10-09 17:48:05 +0530

    commit

--- HackerRank/ConditionalStatements.cpp
+++ HackerRank/ConditionalStatements.cpp
@@ -4,55 +4,54 @@ using namespace std;
 string ltrim(const string &);
 string rtrim(const string &);
 
-
-
 int main()
 {
     string n_temp;
     getline(cin, n_temp);
 
     int n = stoi(ltrim(rtrim(n_temp)));
-    if(n>0&&n<10)
+    if (n > 0 && n < 10)
     {
-        if(n==1)
+        if (n == 1)
         {
-            cout<<"one\n";
+            cout << "one\n";
         }
-        else if(n==2)
+        else if (n == 2)
         {
-            cout<<"two\n";
+            cout << "two\n";
         }
-        else if(n==3)
+        else if (n == 3)
         {
-            cout<<"three\n";
+            cout << "three\n";
         }
-        else if(n==4)
+        else if (n == 4)
         {
-            cout<<"four\n";
+            cout << "four\n";
         }
-        else if(n==5)
+        else if (n == 5)
         {
-            cout<<"five\n";
+            cout << "five\n";
         }
-        else if(n==6)
+        else if (n == 6)
         {
-            cout<<"six\n";
+            cout << "six\n";
         }
-        else if(n==7)
+        else if (n == 7)
         {
-            cout<<"seven\n";
+            cout << "seven\n";
         }
-        else if(n==8)
+        else if (n == 8)
         {
-            cout<<"eight\n";
+            cout << "eight\n";
         }
-        else if(n==9)
+        else if (n == 9)
         {
-            cout<<"nine\n";
+            cout << "nine\n";
         }
     }
-    else{
-        cout<<"Greater than 9";
+    else
+    {
+        cout << "Greater than 9";
     }
 
     // Write your code here
@@ -60,24 +59,24 @@ int main()
     return 0;
 }
 
-string ltrim(const string &str) {
+string ltrim(const string &str)
+{
     string s(str);
 
     s.erase(
         s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
 
     return s;
 }
 
-string rtrim(const string &str) {
+string rtrim(const string &str)
+{
     string s(str);
 
     s.erase(
         find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
+        s.end());
 
     return s;
 }
